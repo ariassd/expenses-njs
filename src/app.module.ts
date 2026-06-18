7be7e59a889d1dd30e141ec9b89e413fb03babd0
Luis Arias <ariassd@gmail.com>
2026-06-18 16:32:24 -0600

    Implement RabbitMQ Integration for Asynchronous Expense Processing
    * AMQP
    
    Rabbit handlers for creation and update expenses

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,13 @@
 import { Logger, Module, OnApplicationShutdown } from '@nestjs/common';
 import { DataSource } from 'typeorm';
-
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Expenses } from './expenses.entity';
 import { ExpensesAggregation } from './expenses-aggregation.entity';
 import { ExpensesController } from './expenses.controller';
 import { ExpensesService } from './expenses.service';
+import { RabbitMQConfig, RabbitMQModule } from '@golevelup/nestjs-rabbitmq';
+import { ExpensesHandler } from './expenses.handler';
 
 @Module({
   imports: [
@@ -23,9 +24,32 @@ import { ExpensesService } from './expenses.service';
       }),
     }),
     TypeOrmModule.forFeature([Expenses, ExpensesAggregation]),
+    RabbitMQModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService): RabbitMQConfig => ({
+        uri: configService.get<string>('AMQP_URI'),
+        prefetchCount: parseInt(configService.get<string>('AMQP_PREFETCH_COUNT') || '10'),
+        connectionInitOptions: { wait: true },
+        exchanges: [
+          {
+            name: configService.get<string>('AMQP_EXCHANGE') || 'com.my_company',
+            type: 'topic',
+          },
+          { name: `${process.env.AMQP_EXCHANGE || 'com.my_company'}.dead_letter`, type: 'fanout' },
+        ],
+        queues: [
+          {
+            name: `${process.env.AMQP_REGISTER_EXPENSES_ROUTING_KEY || 'com.my_company.expenses.create'}.dead_letter`,
+            exchange: `${process.env.AMQP_EXCHANGE || 'com.my_company'}.dead_letter`,
+            routingKey: '',
+          },
+        ],
+      }),
+    }),
   ],
   controllers: [ExpensesController],
-  providers: [ExpensesService],
+  providers: [Logger, ExpensesService, ExpensesHandler],
 })
 export class AppModule implements OnApplicationShutdown {
   private logger = new Logger();
